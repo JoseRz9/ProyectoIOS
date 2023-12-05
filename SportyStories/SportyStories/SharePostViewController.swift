95e337bea6d53aced0ff370457314d59fe1ad82f
Jose David Ramirez Hernandez <47670486+JoseRz9@users.noreply.github.com>
2023-12-04 22:44:12 -0800

    comit
    creacion de ventana luego de crear un video

--- SportyStories/SportyStories/SharePostViewController.swift
+++ SportyStories/SportyStories/SharePostViewController.swift
@@ -10,12 +10,38 @@ import AVFoundation
 
 class SharePostViewController: UIViewController {
     
+    @IBOutlet weak var postButton: UIButton!
+    @IBOutlet weak var draftsButton: UIButton!
+    @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var thumbnailImageView: UIImageView!
+    
+    
     let originalVideoUrl : URL
     var selectedPhoto: UIImage?
+    
+    let placeholder = "Please write a description"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupView()
+        textViewDidChanged()
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(hideKeyboard)))
+    }
+    
+    @objc func hideKeyboard() {
+        self.view.endEditing(true)
+    }
+    
+    //funciones que cambian el estado a solo toda la ventana cuando capturala iamgen
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.tabBarController?.tabBar.isHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.tabBarController?.tabBar.isHidden = false
     }
     
     init?(coder: NSCoder, videoUrl: URL){
@@ -27,4 +53,32 @@ class SharePostViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    func setupView(){
+        draftsButton.layer.borderColor = UIColor.lightGray.cgColor
+        draftsButton.layer.borderWidth = 0.3
+    }
+    
+    
+}
+
+extension SharePostViewController: UITextViewDelegate {
+    func textViewDidChanged() {
+        textView.delegate = self
+        textView.text = placeholder
+        textView.textColor = .lightGray
+    }
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.textColor == .lightGray {
+            textView.text = ""
+            textView.textColor = .black
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text == "" {
+            textView.text = placeholder
+            textView.textColor = .lightGray
+        }
+    }
 }
